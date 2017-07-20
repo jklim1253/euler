a7bd57dab5fb783703ab5c024b189364aa167e11
Jong Kyu, Lim <jklim1253@gmail.com>
2017-07-21 00:42:16 +0900

    update error_code.h
    - set identifier to anonymous enum (error_code).
    - add error_message string array.

--- src/inc/error_code.h
+++ src/inc/error_code.h
@@ -2,14 +2,27 @@
 
 namespace ec
 {
-    enum
+    enum errorcode
     {
         no_error = 0,
         not_implemented_yet,
+        exception_occur,
 
         file_not_found,
         failed_to_load_library_dynamically,
         failed_to_get_createinstance_address,
+
+        end_of_error
+    };
+
+    static const char* error_message[] = {
+        "no_error",
+        "not_implemented_yet",
+        "exception_occur",
+
+        "file_not_found",
+        "failed_to_load_library_dynamically",
+        "failed_to_get_createinstance_address",
     };
 }
 
