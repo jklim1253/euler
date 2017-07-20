e7e7d84b0f70d50ebe414010a2fc522c977c7599
Jong Kyu, Lim <jklim1253@gmail.com>
2017-07-21 00:56:13 +0900

    fix basic_question class
    - export basic_question abstract class.
        - when that class is used in solution cpp files.
    - relocate SOLUTION_API preprocessor.
        - before using for basic_question class.

--- src/inc/basic_question.hpp
+++ src/inc/basic_question.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
-class basic_question
-{
-public :
-    virtual int execute() = 0;
-};
-
 #ifdef SOLUTION_EXPORTS
 #define SOLUTION_API __declspec(dllexport)
 #else
 #define SOLUTION_API __declspec(dllimport)
 #endif
+
+class SOLUTION_API basic_question
+{
+public :
+    virtual int execute() = 0;
+};
 
 extern "C" {
 SOLUTION_API basic_question* CreateInstance();
